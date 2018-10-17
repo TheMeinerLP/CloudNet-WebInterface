335ea16ecd6a73cb6fe31cb39580d40e15de206d
Marc-Antonio Liebe <sqonxxdev@gmail.co>
2018-10-17 22:51:52 +0200

    Code Documentation

--- src/main/java/cloud/waldiekiste/java/projekte/cloudnet/webinterface/services/ErrorService.java
+++ src/main/java/cloud/waldiekiste/java/projekte/cloudnet/webinterface/services/ErrorService.java
@@ -1,6 +1,14 @@
 package cloud.waldiekiste.java.projekte.cloudnet.webinterface.services;
 
+/**
+ * In this class, the errors are listed and there messages are specified.
+ */
+
 public class ErrorService {
+
+    /**
+     * Getting the error code and sending a specified ErrorMessage for the ErrorCode.
+     */
     public static void error(int code){
         System.err.println("Error Code: "+code);
         switch (code){
@@ -8,6 +16,10 @@ public class ErrorService {
                 reason("Config Permission System cannot init");
         }
     }
+
+    /**
+     * Here the Reason is getting name and sended.
+     */
     private static void reason(String s) {
         System.err.println("Reason: "+s);
     }
