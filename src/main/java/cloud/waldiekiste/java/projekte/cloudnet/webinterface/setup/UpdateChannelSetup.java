5f00c41d503b883a3a3ed70c4dd9a81fe4404d44
Marc-Antonio Liebe <sqonxxdev@gmail.co>
2018-10-18 00:01:05 +0200

    Code Documentation & Code CleanUp

--- src/main/java/cloud/waldiekiste/java/projekte/cloudnet/webinterface/setup/UpdateChannelSetup.java
+++ src/main/java/cloud/waldiekiste/java/projekte/cloudnet/webinterface/setup/UpdateChannelSetup.java
@@ -8,13 +8,14 @@
 package cloud.waldiekiste.java.projekte.cloudnet.webinterface.setup;
 
 import cloud.waldiekiste.java.projekte.cloudnet.webinterface.utils.VersionType;
-import de.dytanic.cloudnet.lib.utility.Catcher;
 import de.dytanic.cloudnet.lib.utility.document.Document;
 import de.dytanic.cloudnet.setup.Setup;
 import de.dytanic.cloudnet.setup.SetupRequest;
 import de.dytanic.cloudnet.setup.SetupResponseType;
 import de.dytanic.cloudnetcore.CloudNet;
-
+/*
+In this class, the user chooses the Updatechannel
+ */
 public class UpdateChannelSetup extends Setup {
     public UpdateChannelSetup() {
         setupComplete(t->{
@@ -23,7 +24,14 @@ public class UpdateChannelSetup extends Setup {
             document.append("mdwi.updateChannel",VersionType.valueOf(type).getType());
             CloudNet.getInstance().getDbHandlers().getUpdateConfigurationDatabase().set(document);
         });
-        request(new SetupRequest("type", "Which UpdateChannel you want to use?(RELEASE,DEVELOPMENT,SNAPSHOT)", "This Channel not exsists", SetupResponseType.STRING, s -> {
+        /*
+         Here the user will get a message in which he get asked which UpdateChannel he wants to use RELEASE
+          | DEVELOPMENT | SNAPSHOT
+         If he types a other Message or something other, he gets a ErrorMessage.
+         */
+        request(new SetupRequest("type",
+                "Which UpdateChannel you want to use?(RELEASE,DEVELOPMENT,SNAPSHOT)",
+                "This Channel not exsists", SetupResponseType.STRING, s -> {
             if(s.length() == 0 || s == null || s.isEmpty()){
                 return false;
             }
