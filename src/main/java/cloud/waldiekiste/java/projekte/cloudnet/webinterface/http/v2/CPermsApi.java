04e3b8ee99ac5fb90d0381096cee10ff2988801d
Phillipp Glanz <Phillipp@kallmeier-glanz.de>
2018-10-20 19:49:09 +0200

    Fix UUID Fail

--- src/main/java/cloud/waldiekiste/java/projekte/cloudnet/webinterface/http/v2/CPermsApi.java
+++ src/main/java/cloud/waldiekiste/java/projekte/cloudnet/webinterface/http/v2/CPermsApi.java
@@ -111,7 +111,7 @@ public class CPermsApi extends MethodWebHandlerAdapter {
                             return ResponseUtil.success(fullHttpResponse,false,document);
                         }
                         if (userUUID.matches(
-                                "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"))
+                                "/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i"))
                         {
                             document.append("response",JsonUtil.getGson().toJson(this.projectMain.getCloud().
                                     getDbHandlers().getPlayerDatabase().getPlayer(UUID.fromString(userUUID))));
