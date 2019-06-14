7fd4d610ec4cd603377dc217b7a06ca5700bd63e
theEvilReaper <steffenwx@gmail.com>
2019-06-14 23:03:45 +0200

    Removed unused code and improved code readability

--- src/main/java/cloud/waldiekiste/java/projekte/cloudnet/webinterface/permission/ConfigPermissions.java
+++ src/main/java/cloud/waldiekiste/java/projekte/cloudnet/webinterface/permission/ConfigPermissions.java
@@ -7,78 +7,43 @@
 
 package cloud.waldiekiste.java.projekte.cloudnet.webinterface.permission;
 
-import com.google.gson.reflect.TypeToken;
 import de.dytanic.cloudnet.lib.player.permission.PermissionGroup;
-import de.dytanic.cloudnet.lib.utility.MapWrapper;
-import de.dytanic.cloudnet.lib.utility.Return;
-import de.dytanic.cloudnet.lib.utility.document.Document;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
-import java.nio.file.attribute.FileAttribute;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public final class ConfigPermissions {
+
     private final Path path;
     private Configuration cache;
+
     @SuppressWarnings("unchecked")
-    public ConfigPermissions() throws Exception {
+    public ConfigPermissions() {
         this.path = Paths.get("local/perms.yml");
-        if (!Files.exists(this.path)) {
-            Files.createFile(this.path, (FileAttribute<?>[])new FileAttribute[0]);
-            final Configuration configuration = new Configuration();
-            configuration.set("enabled", true);
-            configuration.set("groups", new Configuration());
-            if (!Files.exists(Paths.get("local/permissions.yml"))) {
-                final PermissionGroup member = new PermissionGroup("default", "","§eMember §7\u258e ", "§f", "§e", 9999, 0, true, new HashMap<>(), MapWrapper.valueableHashMap(new Return<>("Lobby", Collections.singletonList("test.permission.for.group.Lobby"))), new HashMap<>(), new ArrayList<>());
-                this.write(member, configuration);
-                final PermissionGroup admin = new PermissionGroup("Admin", "","§cAdmin §7\u258e ", "§f", "§c", 0, 100, false, (HashMap<String, Boolean>)MapWrapper.valueableHashMap(new Return[] { new Return<>("*", true) }), MapWrapper.valueableHashMap(new Return<>("Lobby", Collections.singletonList("test.permission.for.group.Lobby"))), new HashMap<>(), new ArrayList<>());
-                this.write(admin, configuration);
-            }
-            else {
-                final Document document = Document.loadDocument(Paths.get("local/permissions.yml"));
-                final Collection<PermissionGroup> groups = document.getObject("groups", new TypeToken<Collection<PermissionGroup>>() {}.getType());
-                final Map<String,PermissionGroup> maps = MapWrapper.collectionCatcherHashMap(groups, PermissionGroup::getName);
-                configuration.set("enabled", document.getBoolean("enabled"));
-                for (final PermissionGroup value : maps.values()) {
-                    this.write(value, configuration);
-                }
-                Files.deleteIfExists(Paths.get("local/permissions.yml"));
-            }
-            try (final OutputStream outputStream = Files.newOutputStream(this.path);
-                 final OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)) {
-                ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, outputStreamWriter);
-            }
-        }
         this.loadCache();
     }
+
     public void updatePermissionGroup(final PermissionGroup permissionGroup) {
-        if (this.cache == null) {
-            this.loadCache();
-        }
         this.write(permissionGroup, this.cache);
         try (final OutputStreamWriter outputStreamWriter = new OutputStreamWriter(Files.newOutputStream(this.path), StandardCharsets.UTF_8)) {
             ConfigurationProvider.getProvider(YamlConfiguration.class).save(this.cache, outputStreamWriter);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public Map<String, PermissionGroup> loadAll0() {
-        this.loadCache();
-        return this.read(this.cache);
-    }
-
-    public Map<String, PermissionGroup> loadAll() {
-        if (this.cache == null) {
-            this.loadCache();
-        }
         return this.read(this.cache);
     }
 
@@ -86,8 +51,7 @@ public final class ConfigPermissions {
         try (final InputStream inputStream = Files.newInputStream(this.path);
              final InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
             this.cache = ConfigurationProvider.getProvider(YamlConfiguration.class).load(inputStreamReader);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -142,14 +106,6 @@ public final class ConfigPermissions {
     }
 
     public boolean isEnabled() {
-        this.loadCache();
-        return this.cache.getBoolean("enabled");
-    }
-
-    public boolean isEnabled0() {
-        if (this.cache == null) {
-            this.loadCache();
-        }
         return this.cache.getBoolean("enabled");
     }
 }
