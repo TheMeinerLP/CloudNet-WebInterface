6928a81c1ae66bafb8507631e1808ae6096d570a
theEvilReaper <steffenwx@gmail.com>
2019-06-14 23:04:20 +0200

    Changed dependency injection to a string variable

--- src/main/java/cloud/waldiekiste/java/projekte/cloudnet/webinterface/commands/CommandVersion.java
+++ src/main/java/cloud/waldiekiste/java/projekte/cloudnet/webinterface/commands/CommandVersion.java
@@ -7,20 +7,21 @@
 
 package cloud.waldiekiste.java.projekte.cloudnet.webinterface.commands;
 
-import cloud.waldiekiste.java.projekte.cloudnet.webinterface.ProjectMain;
 import de.dytanic.cloudnet.command.Command;
 import de.dytanic.cloudnet.command.CommandSender;
 
 public final class CommandVersion extends Command {
-    private final ProjectMain projectMain;
-    public CommandVersion(ProjectMain projectMain) {
+
+    private final String version;
+
+    public CommandVersion(String version) {
         super("versionI", "cloudnet.webinterface.version", "vI");
         this.description = "Show you the Module Version of the Material Design Web Interface";
-        this.projectMain = projectMain;
+        this.version = version;
     }
 
     @Override
     public void onExecuteCommand(CommandSender commandSender, String[] strings) {
-        commandSender.sendMessage("Your current extension Version: " +this.projectMain.getVersion());
+        commandSender.sendMessage("Your current extension Version: " + version);
     }
 }
