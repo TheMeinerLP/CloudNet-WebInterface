0a556457a6386eb930652afe39a3d1100c521b15
Phillipp Glanz <p.glanz@madfix.me>
2019-06-17 09:49:26 +0200

    Improved code readability

--- src/main/java/cloud/waldiekiste/java/projekte/cloudnet/webinterface/services/UpdateService.java
+++ src/main/java/cloud/waldiekiste/java/projekte/cloudnet/webinterface/services/UpdateService.java
@@ -25,8 +25,11 @@ import java.util.ArrayList;
 
 public final class UpdateService {
 
+  /**
+   * Check for some update of the module
+   * @param module passes the main class of the module
+   */
   public void checkUpdate(Module module) {
-
     Document document = CloudNet.getInstance().getDbHandlers().getUpdateConfigurationDatabase()
         .get();
     if (document.contains("mdwi.updateChannel")) {
@@ -64,6 +67,10 @@ public final class UpdateService {
     }
   }
 
+  /**
+   * Download the update and replace the old jar
+   * @param data the date of the new update
+   */
   private void update(UpdateData data) {
     try {
       HttpURLConnection httpUrlConnection = (HttpURLConnection) (new URL(data.getFilePath()))
@@ -136,7 +143,7 @@ public final class UpdateService {
       e.printStackTrace();
     }
     String result = null;
-    try(BufferedReader reader = new BufferedReader(
+    try( BufferedReader reader = new BufferedReader(
         new InputStreamReader(connection.getInputStream(),StandardCharsets.UTF_8))) {
       result = reader.readLine();
     } catch (IOException e) {
@@ -149,36 +156,56 @@ public final class UpdateService {
     return data;
   }
 
-  public ArrayList<UpdateData> getUpdates(VersionType branch) throws Exception {
+  /**
+   * Get the update from branch
+   * @param branch The branch they are pull
+   * @return The versions as list
+   */
+  public ArrayList<UpdateData> getUpdates(VersionType branch) {
     String url = String
         .format("https://api.madfix.me/v1/download.php?BRANCH=%s&ENVIRONMENT=CLOUDNET",
             branch.getType());
-    URL adress = new URL(url);
-    HttpURLConnection connection = (HttpURLConnection) adress.openConnection();
-    connection.setRequestProperty("User-Agent",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko)"
-            + " Chrome/23.0.1271.95 Safari/537.11");
-    connection.setConnectTimeout(2000);
-    connection.setDoOutput(false);
-    connection.setDoInput(true);
-
-    if (connection.getResponseCode() == 403) {
-      System.err.println("[Updater] Der Master kann nicht auf die API zugreifen! (403)");
-      return null;
+    URL address = null;
+    try {
+      address = new URL(url);
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
     }
+    try {
+      HttpURLConnection connection = (HttpURLConnection) address.openConnection();
+      connection.setRequestProperty("User-Agent",
+          "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko)"
+              + " Chrome/23.0.1271.95 Safari/537.11");
+      connection.setConnectTimeout(2000);
+      connection.setDoOutput(false);
+      connection.setDoInput(true);
 
-    if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-      System.out.println("Fehler bei der Anfrage");
-      return null;
+      if (connection.getResponseCode() == 403) {
+        System.err.println("[Updater] Der Master kann nicht auf die API zugreifen! (403)");
+        return null;
+      }
+
+      if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+        System.out.println("Fehler bei der Anfrage");
+        return null;
+      }
+
+      String result = null;
+      try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+          connection.getInputStream(), StandardCharsets.UTF_8))) {
+        result = bufferedReader.readLine();
+      }catch (IOException e){
+        e.printStackTrace();
+      }
+      JsonElement jsonObject = new JsonParser().parse(result);
+      ArrayList<UpdateData> datas = new ArrayList<>();
+      jsonObject.getAsJsonObject().get("versions").getAsJsonArray().forEach(
+          t -> datas.add(JsonUtil.getGson().fromJson(t.getAsJsonObject(), UpdateData.class)));
+      connection.disconnect();
+      return datas;
+    }catch (IOException e){
+      e.printStackTrace();
     }
-    String result = new BufferedReader(new InputStreamReader(connection.getInputStream(),
-        StandardCharsets.UTF_8))
-        .readLine();
-    JsonElement jsonObject = new JsonParser().parse(result);
-    ArrayList<UpdateData> datas = new ArrayList<>();
-    jsonObject.getAsJsonObject().get("versions").getAsJsonArray().forEach(
-        t -> datas.add(JsonUtil.getGson().fromJson(t.getAsJsonObject(), UpdateData.class)));
-    connection.disconnect();
-    return datas;
+    return new ArrayList<>();
   }
 }
