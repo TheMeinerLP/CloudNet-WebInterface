042e82f9c1ba8a93dd8e9432ccc3038300576a1d
Phillipp Glanz <p.glanz@madfix.me>
2020-06-26 12:29:47 +0200

    Add missing attributes for the database configuration. #4

--- src/main/java/me/madfix/cloudnet/webinterface/model/DatabaseConfiguration.java
+++ src/main/java/me/madfix/cloudnet/webinterface/model/DatabaseConfiguration.java
@@ -7,6 +7,8 @@ public final class DatabaseConfiguration {
   private final String dataSourceClassName;
   private final String username;
   private final String password;
+  private final String servername;
+  private final String databaseName;
   private final int minimumIdle;
   private final int maximumPoolSize;
   private final long connectionTimeout;
@@ -14,11 +16,15 @@ public final class DatabaseConfiguration {
   private final long maxLifetime;
 
   public DatabaseConfiguration(String dataSourceClassName, String username, String password,
-      int minimumIdle, int maximumPoolSize, long connectionTimeout, long idleTimeout,
+      String servername, String databaseName, int minimumIdle, int maximumPoolSize,
+      long connectionTimeout,
+      long idleTimeout,
       long maxLifetime) {
     this.dataSourceClassName = dataSourceClassName;
     this.username = username;
     this.password = password;
+    this.servername = servername;
+    this.databaseName = databaseName;
     this.minimumIdle = minimumIdle;
     this.maximumPoolSize = maximumPoolSize;
     this.connectionTimeout = connectionTimeout;
@@ -58,6 +64,23 @@ public final class DatabaseConfiguration {
     return maxLifetime;
   }
 
+
+  @Override
+  public String toString() {
+    return "DatabaseConfiguration{" +
+        "dataSourceClassName='" + dataSourceClassName + '\'' +
+        ", username='" + username + '\'' +
+        ", password='" + password + '\'' +
+        ", servername='" + servername + '\'' +
+        ", databaseName='" + databaseName + '\'' +
+        ", minimumIdle=" + minimumIdle +
+        ", maximumPoolSize=" + maximumPoolSize +
+        ", connectionTimeout=" + connectionTimeout +
+        ", idleTimeout=" + idleTimeout +
+        ", maxLifetime=" + maxLifetime +
+        '}';
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -74,26 +97,15 @@ public final class DatabaseConfiguration {
         maxLifetime == that.maxLifetime &&
         Objects.equals(dataSourceClassName, that.dataSourceClassName) &&
         Objects.equals(username, that.username) &&
-        Objects.equals(password, that.password);
+        Objects.equals(password, that.password) &&
+        Objects.equals(servername, that.servername) &&
+        Objects.equals(databaseName, that.databaseName);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(dataSourceClassName, username, password, minimumIdle, maximumPoolSize,
-        connectionTimeout, idleTimeout, maxLifetime);
-  }
-
-  @Override
-  public String toString() {
-    return "DatabaseConfiguration{" +
-        "dataSourceClassName='" + dataSourceClassName + '\'' +
-        ", username='" + username + '\'' +
-        ", password='" + password + '\'' +
-        ", minimumIdle=" + minimumIdle +
-        ", maximumPoolSize=" + maximumPoolSize +
-        ", connectionTimeout=" + connectionTimeout +
-        ", idleTimeout=" + idleTimeout +
-        ", maxLifetime=" + maxLifetime +
-        '}';
+    return Objects
+        .hash(dataSourceClassName, username, password, servername, databaseName, minimumIdle,
+            maximumPoolSize, connectionTimeout, idleTimeout, maxLifetime);
   }
 }
