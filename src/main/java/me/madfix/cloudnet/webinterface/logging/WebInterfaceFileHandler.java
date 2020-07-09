00b327f3d78e6f421544e3036b57bcf63b3aca72
TheMeinerLP <p.glanz@madfix.me>
2020-07-09 13:25:42 +0200

    Remove formatter and improve count and limit

--- src/main/java/me/madfix/cloudnet/webinterface/logging/WebInterfaceFileHandler.java
+++ src/main/java/me/madfix/cloudnet/webinterface/logging/WebInterfaceFileHandler.java
@@ -3,14 +3,10 @@ package me.madfix.cloudnet.webinterface.logging;
 
 import java.io.IOException;
 import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.XMLFormatter;
 
 public class WebInterfaceFileHandler extends FileHandler {
 
     public WebInterfaceFileHandler() throws IOException {
-        super("wi/log/module.%g.xml", 2, 1, false);
-        setFormatter(new XMLFormatter());
-        setLevel(Level.ALL);
+        super("wi/log/module.%g.xml", 80000, 10, false);
     }
 }
