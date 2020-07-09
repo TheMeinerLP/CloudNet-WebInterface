4180f3eb0d038e7a3905d3f4baf05af4a3583225
TheMeinerLP <p.glanz@madfix.me>
2020-07-09 13:26:24 +0200

    Remove resource bundle support. Add error messages and improve code.

--- src/main/java/me/madfix/cloudnet/webinterface/logging/WebInterfaceLogger.java
+++ src/main/java/me/madfix/cloudnet/webinterface/logging/WebInterfaceLogger.java
@@ -3,16 +3,18 @@ package me.madfix.cloudnet.webinterface.logging;
 import de.dytanic.cloudnetcore.CloudNet;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+import java.util.logging.XMLFormatter;
 
 public class WebInterfaceLogger extends Logger {
     public WebInterfaceLogger() {
-        super("WebInterfaceLogger", "logger_messages");
+        super("WebInterfaceLogger", null);
         setLevel(Level.INFO);
         setParent(CloudNet.getLogger());
         Path path = Paths.get("wi", "log");
@@ -20,13 +22,23 @@ public class WebInterfaceLogger extends Logger {
             try {
                 Files.createDirectories(path);
             } catch (IOException e) {
-                log(Level.SEVERE,"logger.handler.create.folder", e);
+                log(Level.SEVERE,"An unexpected error occurred while creating the file logger folder", e);
             }
         }
         try {
-            addHandler(new WebInterfaceFileHandler());
+            WebInterfaceFileHandler webInterfaceFileHandler = new WebInterfaceFileHandler();
+            webInterfaceFileHandler.setLevel(Level.ALL);
+            webInterfaceFileHandler.setFormatter(new XMLFormatter());
+            webInterfaceFileHandler.setEncoding(StandardCharsets.UTF_8.name());
+            addHandler(webInterfaceFileHandler);
         } catch (IOException e) {
-            log(Level.SEVERE,"logger.handler.add.file", e);
+            log(Level.SEVERE,"An unexpected error occurred while adding the file logger", e);
         }
+    }
+
+    @Override
+    public void log(LogRecord record) {
+        record.setMessage(String.format("[%s] %s", "WI", record.getMessage()));
+        super.log(record);
     }
 }
