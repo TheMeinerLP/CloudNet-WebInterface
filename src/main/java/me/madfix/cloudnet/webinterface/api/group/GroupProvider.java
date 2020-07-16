1c8f4844c3d846720fd6e18242d79c52eb74620c
TheMeinerLP <p.glanz@madfix.me>
2020-07-16 10:32:33 +0200

    Add skeleton schema for group provider for 1.9 Update.

---
+++ src/main/java/me/madfix/cloudnet/webinterface/api/group/GroupProvider.java
@@ -0,0 +1,12 @@
+package me.madfix.cloudnet.webinterface.api.group;
+
+import me.madfix.cloudnet.webinterface.WebInterface;
+
+public final class GroupProvider {
+
+    private final WebInterface webInterface;
+
+    public GroupProvider(WebInterface webInterface) {
+        this.webInterface = webInterface;
+    }
+}
